1a8935c6245fd880bf3062e39d799ac0b12feb81
mathiast3 <mathiasjtaylor@gmail.com>
2018-10-19 19:38:16 -0700

    Finished project

--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 require 'sinatra'
  
  class MyApp < Sinatra::Base
+  enable :sessions
+  
   get '/' do
     '<!DOCTYPE html><html><head><meta charset="utf-8"><title>World</title></head><body><h1>Goodbye World</h1></body></html>'
+  end
+
+  get '/new' do
+    '<!DOCTYPE html><html><head><meta charset="utf-8"><title>World</title></head><body><h1>Strat new game</h1></body></html>'
   end
 end
